68e8b8aec566d6dc12abb9bca96851fdd5c81ebc
Iván Villegas <villeiv@gmail.com>
2025-12-16 14:23:27 -0600

    log con scroll interno

--- features/CartaPorte/components/CartaPorteLog.tsx
+++ features/CartaPorte/components/CartaPorteLog.tsx
@@ -3,12 +3,19 @@ import CartaPorteNoDataEvent from "./CartaPorteNoDataEvent";
 import CartaPorteStageEvent from "./CartaPorteStageEvent";
 import {Button, Dialog, DialogHeader, DialogContent, DialogDescription, DialogTitle, DialogFooter} from "@traxion-global/design-system/react";
 
+const styles = {
+    content:"max-h-[90vh] flex flex-col",
+    scrollableContainer:"flex-1 overflow-y-auto px-1"
+};
+
 function CartaPorteLog({ open, setOpen, logInfo}){
     return <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
+        <DialogContent className={styles.content}>
             <DialogHeader>
                 <DialogTitle>{logInfo.title}</DialogTitle>
                 <DialogDescription>{logInfo.description}</DialogDescription>
+            </DialogHeader>
+            <div className={styles.scrollableContainer}>
                 {
                     logInfo.stages &&
                     logInfo.stages.map((stage, index) => (
@@ -25,7 +32,7 @@ function CartaPorteLog({ open, setOpen, logInfo}){
                         </CartaPorteStageEventsWrapper>
                     ))
                 }
-            </DialogHeader>
+            </div>
             <DialogFooter>
                 <Button variant={"secondary"} size={"sm"} onClick={_=>setOpen(false)}>Aceptar</Button>
             </DialogFooter>
